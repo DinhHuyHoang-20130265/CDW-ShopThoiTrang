3906bd0f0b8b120f780528a6bd09b5fd2ecbdfdb
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2024-03-15 15:43:39 +0700

    update: finished feature icon

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Slider from "../../wrappers/Slider";
+import FeatureIcon from "../../wrappers/feature-icon/FeatureIcon";
 
 const Home = () => {
     return (
         <div>
             <Slider/>
 
-            {/*/!* featured icon *!/*/}
-            {/*<FeatureIcon spaceTopClass="pt-100" spaceBottomClass="pb-60" />*/}
+            <FeatureIcon spaceTopClass="pt-100" spaceBottomClass="pb-60" />
 
             {/*/!* tab product *!/*/}
             {/*<TabProduct spaceBottomClass="pb-60" category="fashion" />*/}
