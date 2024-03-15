0b8ccd110764d0d5d01322259d4e90b06e301813
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2024-03-15 17:04:47 +0700

    update: finished productHome, product Quick view icon

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Slider from "../../wrappers/Slider";
 import FeatureIcon from "../../wrappers/feature-icon/FeatureIcon";
+import TabProduct from "../../wrappers/product/TabProduct";
 
 const Home = () => {
     return (
         <div>
             <Slider/>
 
+            {/* featured icon */}
             <FeatureIcon spaceTopClass="pt-100" spaceBottomClass="pb-60" />
 
-            {/*/!* tab product *!/*/}
-            {/*<TabProduct spaceBottomClass="pb-60" category="fashion" />*/}
+            {/* tab product */}
+            <TabProduct spaceBottomClass="pb-60" category="fashion" />
 
             {/*/!* blog featured *!/*/}
             {/*<BlogFeatured spaceBottomClass="pb-55" />*/}
