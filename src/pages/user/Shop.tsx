93c1240d5bd4ad0c50cdb3a80ef5f510f3911863
nnhuy20130281 <20130281@st.hcmuaf.edu.vn>
2024-03-15 22:17:42 +0700

    updated: Shop

--- src/pages/user/Shop.tsx
+++ src/pages/user/Shop.tsx
@@ -1,11 +1,106 @@
-import React  from "react";
+import React, {Fragment, useEffect, useState} from "react";
+import {BreadcrumbsItem} from "react-breadcrumbs-dynamic";
+import ShopSidebar from "../../components/shop/ShopSidebar";
+import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
+import {connect} from "react-redux";
+import {getSortedProducts} from "../../helpers/product";
+import ShopTopbar from "../../components/shop/ShopTopbar";
+import PropTypes from "prop-types";
+import ShopProducts from "../../components/shop/ShopProducts";
+// import Paginator from 'react-hooks-paginator';
 
-const Shop = () => {
+
+const Shop = ({location, products} : any) => {
+    const [layout, setLayout] = useState('grid three-column');
+    const [sortType, setSortType] = useState('');
+    const [sortValue, setSortValue] = useState('');
+    const [filterSortType, setFilterSortType] = useState('');
+    const [filterSortValue, setFilterSortValue] = useState('');
+    const [offset, setOffset] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentData, setCurrentData] = useState([]);
+    const [sortedProducts, setSortedProducts] = useState([]);
+
+    const pageLimit = 15;
+    const {pathname} = location;
+
+    const getLayout = (layout : any) => {
+        setLayout(layout)
+    }
+
+    const getSortParams = (sortType : any , sortValue : any) => {
+        setSortType(sortType);
+        setSortValue(sortValue);
+    }
+
+    const getFilterSortParams = (sortType : any, sortValue : any) => {
+        setFilterSortType(sortType);
+        setFilterSortValue(sortValue);
+    }
+
+    useEffect(() => {
+        let sortedProducts :any = getSortedProducts(products, sortType, sortValue);
+        const filterSortedProducts = getSortedProducts(sortedProducts, filterSortType, filterSortValue);
+        sortedProducts = filterSortedProducts;
+        setSortedProducts(sortedProducts);
+        setCurrentData(sortedProducts.slice(offset, offset + pageLimit));
+    }, [offset, products, sortType, sortValue, filterSortType, filterSortValue]);
+
     return (
-        <div>
-            Shop
-        </div>
-    );
-};
+        <Fragment>
 
-export default Shop;
+            <BreadcrumbsItem to={process.env.PUBLIC_URL + '/'}>Home</BreadcrumbsItem>
+            <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>Shop</BreadcrumbsItem>
+
+            {/* breadcrumb */}
+            <Breadcrumb/>
+
+            <div className="shop-area pt-95 pb-100">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3 order-2 order-lg-1">
+                            {/* shop sidebar */}
+                            <ShopSidebar products={products} getSortParams={getSortParams} sideSpaceClass="mr-30"/>
+                        </div>
+                        <div className="col-lg-9 order-1 order-lg-2">
+                            {/* shop topbar default */}
+                            <ShopTopbar getLayout={getLayout} getFilterSortParams={getFilterSortParams}
+                                        productCount={products.length} sortedProductCount={currentData.length}/>
+
+                            {/* shop page content default */}
+                            <ShopProducts layout={layout} products={currentData}/>
+
+                            {/* shop product pagination */}
+                            <div className="pro-pagination-style text-center mt-30">
+                                {/*<Paginator*/}
+                                {/*    totalRecords={sortedProducts.length}*/}
+                                {/*    pageLimit={pageLimit}*/}
+                                {/*    pageNeighbours={2}*/}
+                                {/*    setOffset={setOffset}*/}
+                                {/*    currentPage={currentPage}*/}
+                                {/*    setCurrentPage={setCurrentPage}*/}
+                                {/*    pageContainerClass="mb-0 mt-0"*/}
+                                {/*    pagePrevText="«"*/}
+                                {/*    pageNextText="»"*/}
+                                {/*/>*/}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+Shop.propTypes = {
+    location: PropTypes.object,
+    products: PropTypes.array
+}
+
+const mapStateToProps = (state : any) => {
+    return {
+        products: state.productData.products
+    }
+}
+
+export default connect(mapStateToProps)(Shop);
