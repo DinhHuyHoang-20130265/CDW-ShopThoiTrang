a410ca64e704f75fb814b67425b30143b410c8bd
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2024-03-16 00:27:04 +0700

    update: fixed shop

--- src/components/shop/ShopProducts.tsx
+++ src/components/shop/ShopProducts.tsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import React from "react";
-import ProductGrid from "../../wrappers/product/ProductGrid";
+import ProductGridForShop from "../../wrappers/product/ProductGridForShop";
 
-const ShopProducts = ({ products , layout } : any) => {
+const ShopProducts = ({products, layout}: any) => {
     return (
         <div className="shop-bottom-area mt-35">
             <div className={`row ${layout ? layout : ""}`}>
-                <ProductGrid products={products} spaceBottomClass="mb-25" />
+                <ProductGridForShop products={products} spaceBottomClass="mb-25"/>
             </div>
         </div>
     );
