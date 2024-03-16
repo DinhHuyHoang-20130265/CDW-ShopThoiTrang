28b354fe57dff464a99e82bd0c5412f03430f13d
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2024-03-17 00:42:56 +0700

    update: fixed product details => finished this page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,6 +5,7 @@ import About from "../pages/user/About";
 import Shop from "../pages/user/Shop";
 import DashBoard from "../pages/admin/DashBoard";
 import React from "react";
+import ProductDetailRoute from "./ProductDetailRoute";
 
 export const webRouter = createBrowserRouter([
     {
@@ -23,7 +24,16 @@ export const webRouter = createBrowserRouter([
             }, {
                 path: "shop",
                 element: <Shop/>,
-            }, {
+
+            }
+            , {
+                path: "product/:id",
+                element: <ProductDetailRoute/>,
+                loader: async (routeProps: any) => {
+                    return routeProps;
+                }
+            }
+            , {
                 path: "admin",
                 element: <DashBoard/>,
                 children: [
