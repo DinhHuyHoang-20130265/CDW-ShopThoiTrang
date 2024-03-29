035082ca06c6d1de748d388c990578940f8d8902
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2024-03-29 23:01:48 +0700

    updated: fix logic that the backend should be refresh the token

--- FrontEnd-Admin/src/authProvider.tsx
+++ FrontEnd-Admin/src/authProvider.tsx
@@ -1,24 +1,47 @@
 import axios from 'axios';
 import {AuthProvider} from 'react-admin';
+
+
+const httpClient = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+});
+
+httpClient.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+            // @ts-ignore
+            authProvider.logout().then(r => console.log(r));
+            window.location.href = '/#/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export const authProvider: AuthProvider = {
+
     login: async ({username, password}) => {
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login-admin`, {username, password}, {
+        await httpClient.post(`${process.env.REACT_APP_API_URL}/auth/login-admin`, {username, password}, {
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
             },
             withCredentials: true
-        });
-        if (response.status === 200) {
-            console.log(response)
-            localStorage.setItem('admin', JSON.stringify(response.data));
-            window.location.href = '/';
-        } else {
+        }).then((response) => {
+            if (response.status === 200) {
+                console.log(response)
+                localStorage.setItem('admin', JSON.stringify(response.data));
+                window.location.href = '/';
+            }
+        }).catch((error) => {
+            if (error.status === 401) { // @ts-ignore
+                return authProvider.logout();
+            }
             return Promise.reject();
-        }
+        });
     },
     logout: async () => {
-        await axios.post(`${process.env.REACT_APP_API_URL}/auth/sign-out`, {}, {
+        await httpClient.post(`${process.env.REACT_APP_API_URL}/auth/sign-out`, {}, {
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
@@ -33,37 +56,50 @@ export const authProvider: AuthProvider = {
     checkAuth: () =>
         localStorage.getItem('admin') ? Promise.resolve() : Promise.reject(),
     getPermissions: async () => {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/user/get-authorities`, {
+        await httpClient.get(`${process.env.REACT_APP_API_URL}/user/get-authorities`, {
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
             },
             withCredentials: true
-        });
-        if (response.status === 200) {
-            return Promise.resolve(response.data);
-        } else {
+        }).then((response) => {
+            if (response.status === 200) {
+                return Promise.resolve(response.data);
+            }
+        }).catch((error) => {
+            if (error.status === 401) { // @ts-ignore
+                return authProvider.logout();
+            }
             return Promise.reject();
-        }
+        });
+
     },
+    //@ts-ignore
     getIdentity: async () => {
-        const response: any = await axios.get(`${process.env.REACT_APP_API_URL}/user/info`, {
+        await httpClient.get(`${process.env.REACT_APP_API_URL}/user/info`, {
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
             },
             withCredentials: true
-        });
-        if (response.status === 200) {
-            return Promise.resolve({
-                id: "admin",
-                fullName: response.fullName,
-                email: response.email,
-                phone: response.phone,
-                avt: response.avtUrl
-            });
-        } else {
+        }).then((response: any) => {
+            console.log(response)
+            if (response.status === 200) {
+                return Promise.resolve({
+                    id: "admin",
+                    fullName: response.fullName,
+                    email: response.email,
+                    phone: response.phone,
+                    avt: response.avtUrl
+                });
+            } else console.log(response.status)
+        }).catch((error) => {
+            if (error.status === 401) {
+                console.log("abvc")
+                // @ts-ignore
+                return authProvider.logout();
+            }
             return Promise.reject();
-        }
+        });
     }
 }
--- FrontEnd-Admin/src/dataProvider/dataProvider.tsx
+++ FrontEnd-Admin/src/dataProvider/dataProvider.tsx
@@ -1,34 +1,43 @@
 import {DataProvider, fetchUtils} from 'react-admin';
+import {authProvider} from "../authProvider";
 
 const httpClient = fetchUtils.fetchJson;
 
-export const dataProvider: DataProvider = {
-    getList: async (resource: any, params: any) => {
-        const {page, perPage} = params.pagination;
-        const {field, order} = params.sort;
-        const query = {
-            filter: JSON.stringify(fetchUtils.flattenObject(params.filter)),
-            sort: field,
-            order: order,
-            start: (page - 1) * perPage,
-            end: page * perPage,
-        };
 
-        const {
-            headers,
-            json
-        }: any = await httpClient(`${process.env.REACT_APP_API_URL}/${resource}?${fetchUtils.queryParameters(query)}`, {
-            method: 'GET',
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            }),
-            credentials: 'include',
-        });
-        return ({
-            data: json.content,
-            total: parseInt(json.totalElements, 10),
-        });
+export const dataProvider: DataProvider = {
+    // @ts-ignore
+    getList: async (resource: any, params: any) => {
+        try {
+            const {page, perPage} = params.pagination;
+            const {field, order} = params.sort;
+            const query = {
+                filter: JSON.stringify(fetchUtils.flattenObject(params.filter)),
+                sort: field,
+                order: order,
+                start: (page - 1) * perPage,
+                end: page * perPage,
+            };
+
+            const {json} = await httpClient(`${process.env.REACT_APP_API_URL}/${resource}?${fetchUtils.queryParameters(query)}`, {
+                method: 'GET',
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                }),
+                credentials: 'include',
+            });
+
+            return {
+                data: json.content,
+                total: parseInt(json.totalElements, 10),
+            };
+        } catch (error: any) {
+            if (error.status === 401) {
+                // @ts-ignore
+                await authProvider.logout();
+                window.location.href = '/#/login';
+            }
+        }
     },
 
     getOne: (resource: any, params: any) =>
@@ -44,19 +53,29 @@ export const dataProvider: DataProvider = {
         })),
     getMany: (resource: any, params: any) => Promise.resolve({data: []}),
     getManyReference: (resource: any, params: any) => Promise.resolve({data: []}),
-    create: (resource: any, params: any) =>
-        httpClient(`${process.env.REACT_APP_API_URL}/${resource}`, {
-            method: 'POST',
-            body: JSON.stringify(params.data),
+    // @ts-ignore
+    create: (resource: any, params: any) => {
+        try {
+            httpClient(`${process.env.REACT_APP_API_URL}/${resource}`, {
+                method: 'POST',
+                body: JSON.stringify(params.data),
 
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            }),
-            credentials: 'include'
-        }).then(({json}) => ({
-            data: {...params.data, id: json.id},
-        })),
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                }),
+                credentials: 'include'
+            }).then(({json}) => ({
+                data: {...params.data, id: json.id},
+            }));
+        } catch (error: any) {
+            if (error.status === 401) {
+                // @ts-ignore
+                authProvider.logout().then(r => console.log(r));
+                window.location.href = '/#/login';
+            }
+        }
+    },
 
     update: (resource: any, params: any) =>
         httpClient(`${process.env.REACT_APP_API_URL}/${resource}/${params.id}`, {
