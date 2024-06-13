ef7f2baab25719e28e6b9ad452940853e48e777e
Đinh Huy Hoàng <dinh37823@gmail.com>
2024-06-14 00:15:20 +0700

    update: finished responsive promotion

--- FrontEnd-Admin/src/promotion/PromotionList.tsx
+++ FrontEnd-Admin/src/promotion/PromotionList.tsx
@@ -7,12 +7,13 @@ import {
     EditButton,
     ExportButton,
     FilterButton, FunctionField,
-    List, NullableBooleanInput, NumberField,
+    List, NullableBooleanInput, NumberField, RaThemeOptions,
     SelectColumnsButton,
     TextField,
-    TextInput,
-    TopToolbar, useListController,
+    TextInput, ThemeSetter, ThemeType,
+    TopToolbar, useListController, useTheme,
 } from 'react-admin';
+import {Theme, useMediaQuery} from "@mui/material";
 
 const ListActions = () => (
     <TopToolbar>
@@ -29,60 +30,82 @@ const postFilters = () => [
 ];
 
 const PromotionList = () => {
-    const {data, isLoading}: any = useListController();
+        const {data, isLoading}: any = useListController();
+        if (isLoading) return null;
 
-    if (isLoading) return null;
+        const checkPromotionStatus = (promotion: any) => {
+            const currentDate = new Date();
+            const startDate = new Date(promotion.startDate);
+            const endDate = new Date(promotion.endDate);
 
-    const checkPromotionStatus = (promotion: any) => {
-        const currentDate = new Date();
-        const startDate = new Date(promotion.startDate);
-        const endDate = new Date(promotion.endDate);
-
-        if (currentDate < startDate) {
-            return 'Chưa diễn ra';
-        } else if (currentDate >= startDate && currentDate <= endDate) {
-            return 'Đang diễn ra';
-        } else {
-            return 'Đã kết thúc';
-        }
-    };
-
-    return (
-        data ? (
-            <List
-                sort={{field: 'name', order: 'ASC'}}
-                perPage={20}
-                pagination={false}
-                component="div"
-                actions={<ListActions/>}
-                filters={postFilters()}
-            >
-                <DatagridConfigurable>
-                    <TextField source="id" label={"Mã"}/>
-                    <TextField source="name" label={"Tên"}/>
-                    <DateField source="startDate" label={"Ngày bắt đầu"}/>
-                    <DateField source="endDate" label={"Ngày kết thúc"}/>
-                    <BooleanField source="status" label={"Trạng thái"}/>
-                    <FunctionField
-                        label="Hoạt động"
-                        render={(record: any) => (
-                            <span>{checkPromotionStatus(record)}</span>
-                        )}
-                    />
-                    <NumberField source="discount" label={"Giảm giá"}/>
-                    <FunctionField
-                        label="SL sản phẩm"
-                        render={(record: any) => (
-                            record.products &&
-                            <span>{record.products.length}</span>
-                        )}
-                    />
-                    <EditButton/>
-                </DatagridConfigurable>
-            </List>
-        ) : <div>Không có khuyến mãi</div>
-    );
-};
+            if (currentDate < startDate) {
+                return 'Chưa diễn ra';
+            } else if (currentDate >= startDate && currentDate <= endDate) {
+                return 'Đang diễn ra';
+            } else {
+                return 'Đã kết thúc';
+            }
+        };
+        return (
+            data ? (
+                    <List
+                        sort={{field: 'name', order: 'ASC'}}
+                        perPage={20}
+                        pagination={false}
+                        component="div"
+                        actions={<ListActions/>}
+                        filters={postFilters()}
+                        sx={{
+                            '@media(max-width:900px)': {
+                                '.RaList-main > .RaList-actions': {
+                                    display: 'block',
+                                    '.MuiToolbar-root.MuiToolbar-dense': {
+                                        float: 'left'
+                                    }
+                                }
+                            },
+                            '@media(max-width:600px)': {
+                                '.RaList-main > .RaList-actions': {
+                                    display: 'block',
+                                    '.MuiToolbar-root.MuiToolbar-regular': {
+                                        float: 'left'
+                                    }
+                                }
+                            }
+                        }}
+                    >
+                        <DatagridConfigurable>
+                            < TextField
+                                source="id"
+                                label={"Mã"}
+                            />
+                            <TextField source="name" label={"Tên"}/>
+                            <DateField source="startDate" label={"Ngày bắt đầu"}/>
+                            <DateField source="endDate" label={"Ngày kết thúc"}/>
+                            <BooleanField source="status" label={"Trạng thái"}/>
+                            <FunctionField
+                                label="Hoạt động"
+                                render={(record: any) => (
+                                    <span>{checkPromotionStatus(record)}</span>
+                                )}
+                            />
+                            <NumberField source="discount" label={"Giảm giá"}/>
+                            <FunctionField
+                                label="SL sản phẩm"
+                                render={(record: any) => (
+                                    record.products &&
+                                    <span>{record.products.length}</span>
+                                )}
+                            />
+                            <EditButton/>
+                        </DatagridConfigurable>
+                    </List>
+                ) :
+                <div>Không có khuyến mãi</div>
+        )
+            ;
+    }
+;
 
 
 export default PromotionList;
